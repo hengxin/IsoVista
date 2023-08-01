69e1454154628932e0df0249297f0830029f6656
NayameoW <77333729+NayameoW@users.noreply.github.com>
2023-08-01 13:48:17 -0500

    test: complete test for HistoryLoaderSerializerTest

--- src/test/java/HistoryLoaderSerializerTest.java
+++ src/test/java/HistoryLoaderSerializerTest.java
@@ -1,8 +1,33 @@
+import history.History;
+import history.loader.TextHistoryLoader;
+import history.serializer.TextHistorySerializer;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HistoryLoaderSerializerTest {
     @Test
     public void testHistoryLoaderAndSerializer() {
+        String currentDirectory = System.getProperty("user.dir") + "\\src\\test\\";
+        String tempFilePath = currentDirectory + "temp_test_history.txt";
 
+        TextHistoryLoader historyLoader = new TextHistoryLoader();
+        History<Long, Long> history = historyLoader.loadHistory(currentDirectory + "sample_history.txt");
+
+        TextHistorySerializer historySerializer = new TextHistorySerializer();
+        historySerializer.serializeHistory(history, tempFilePath);
+
+        History<Long, Long> loadedHistory = historyLoader.loadHistory(tempFilePath);
+
+        assertEquals(history, loadedHistory);
+
+         File tempFile = new File(tempFilePath);
+         if (tempFile.delete()) {
+            System.out.println("Successfully deleted the temporary file");
+         } else {
+            System.out.println("Failed to delete the temporary file");
+         }
     }
 }
