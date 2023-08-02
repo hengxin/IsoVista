f8749384b642b43dab5ae112378c812afb520560
NayameoW <xtc1207445468@outlook.com>
2023-08-02 12:44:11 -0500

    style: modify comments in TextHistorySerializer

--- src/main/java/history/serializer/TextHistorySerializer.java
+++ src/main/java/history/serializer/TextHistorySerializer.java
@@ -7,23 +7,21 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * A serializer that writes a history to a text file.
+ */
 public class TextHistorySerializer implements HistorySerializer<Long, Long> {
     @Override
     public void serializeHistory(History<Long, Long> history, String path) {
         try (var out = new BufferedWriter(new FileWriter(path))) {
-            // Step 1: Loop through all transactions in the history
             for (var txn : history.getFlatTransactions()) {
-                // Step 2: Loop through all operations in each transaction
                 for (var op : txn.getOps()) {
-                    // Step 3: Determine the operation type (READ or WRITE)
                     String opType = op.getType() == Operation.Type.READ ? "r" : "w";
-                    // Step 4: Get the key and value of the operation
                     long key = op.getKey();
                     long value = op.getValue();
-                    // Step 5: Get the session and transaction ID of the operation
                     long session = txn.getSession().getId();
                     long txnId = txn.getId();
-                    // Step 7: Write the operation in the specified format to the file
+
                     // txnId == -1 => aborted
                     if (txnId == -1) {
                         out.write(String.format("w(%d,%d,%d,-1)%n", key, value, session));
@@ -33,7 +31,6 @@ public class TextHistorySerializer implements HistorySerializer<Long, Long> {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
