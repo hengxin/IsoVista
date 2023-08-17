d28a867da8cbb785bc5169be8a77219c064b6452
Tiancheng Xing <77333729+NayameoW@users.noreply.github.com>
2023-08-17 09:15:40 +0800

    TextHistorySerializer and tests (#10)
    * test: generate test cases of History
    
    * test: complete test for HistoryLoaderSerializerTest
    
    * feat: Implement TextHistorySerializer class
    
    * style: modify comments in TextHistorySerializer
    
    * test: add sample_history file for testing
    
    * test: fix the path problem
    
    * fix: problems in TextHistorySerializer
    
    * test: update sample history

--- src/main/java/history/serializer/TextHistorySerializer.java
+++ src/main/java/history/serializer/TextHistorySerializer.java
@@ -1,10 +1,31 @@
 package history.serializer;
 
 import history.History;
+import history.Operation;
+import lombok.SneakyThrows;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+/**
+ * A serializer that writes a history to a text file.
+ */
 public class TextHistorySerializer implements HistorySerializer<Long, Long> {
     @Override
+    @SneakyThrows
     public void serializeHistory(History<Long, Long> history, String path) {
+        try(var out = new BufferedWriter(new FileWriter(path))) {
+            for (var txn : history.getFlatTransactions()) {
+                for (var op : txn.getOps()) {
+                    String opType = op.getType() == Operation.Type.READ ? "r" : "w";
+                    long key = op.getKey();
+                    long value = op.getValue();
+                    long session = txn.getSession().getId();
+                    long txnId = txn.getId();
 
+                    out.write(String.format("%s(%d,%d,%d,%d)%n", opType, key, value, session, txnId));
+                }
+            }
+        }
     }
 }
