ffe7119cdae77d595667fd0f59ebe2a12cb3adf5
Draco <dracode01@gmail.com>
2023-08-17 01:13:55 +0000

    Run sessions concurrently
    Signed-off-by: Draco <dracode01@gmail.com>

--- src/main/java/generator/general/GeneralGenerator.java
+++ src/main/java/generator/general/GeneralGenerator.java
@@ -3,11 +3,14 @@ package generator.general;
 import generator.Generator;
 import history.History;
 import history.Operation;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.RandomUtils;
 
-import java.util.HashMap;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GeneralGenerator implements Generator<Long, Long> {
     private long session;
@@ -28,31 +31,43 @@ public class GeneralGenerator implements Generator<Long, Long> {
         this.random = new Random();
     }
 
+    @SneakyThrows
     @Override
     public History<Long, Long> generate() {
         var history = new History<Long, Long>();
-        var counts = new HashMap<Long, Long>();
+        var counts = new ConcurrentHashMap<Long, Long>();
+        ExecutorService executor = Executors.newFixedThreadPool((int) this.session);
+        var todo = new ArrayList<Callable<Void>>();
         for (var iSession = 0; iSession < this.session; iSession++) {
+            int finalISession = iSession;
             history.addSession(iSession);
-            for (var iTxn = 0; iTxn < this.transaction; iTxn++) {
-                var txnId = iSession * this.transaction + iTxn;
-                history.addTransaction(history.getSession(iSession), txnId);
-                for (var iOp = 0; iOp < this.operation; iOp++) {
-                    Operation.Type type;
-                    if (random.nextDouble() < this.readProportion) {
-                        type = Operation.Type.READ;
-                    } else {
-                        type = Operation.Type.WRITE;
+            Callable<Void> task = () -> {
+                for (var iTxn = 0; iTxn < transaction; iTxn++) {
+                    var txnId = finalISession * transaction + iTxn;
+                    synchronized (history) {
+                        history.addTransaction(history.getSession(finalISession), txnId);
                     }
-                    // TODO: impl distribution
-                    var key = RandomUtils.nextLong(1, this.key + 1);
-                    var val = counts.getOrDefault(key, 0L);
-                    counts.put(key, val + 1);
-                    history.addOperation(history.getTransaction(txnId), type, key, val);
+                    for (var iOp = 0; iOp < operation; iOp++) {
+                        Operation.Type type;
+                        if (random.nextDouble() < readProportion) {
+                            type = Operation.Type.READ;
+                        } else {
+                            type = Operation.Type.WRITE;
+                        }
+                        // TODO: impl distribution
+                        var var = RandomUtils.nextLong(1, key + 1);
+                        var val = counts.getOrDefault(var, 0L);
+                        counts.put(var, val + 1);
+                        synchronized (history) {
+                            history.addOperation(history.getTransaction(txnId), type, var, val);
+                        }
+                    }
                 }
-            }
+                return null;
+            };
+            todo.add(task);
         }
-
+        executor.invokeAll(todo);
         return history;
     }
 }
