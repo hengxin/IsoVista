599c0a7cce2062ecee4bf3d7f9d0d81f37f2c9f6
NayameoW <naya1207445468@foxmail.com>
2023-09-16 22:19:58 +0800

    fix: update to the latest version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,14 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.io.FileWriter;
+import java.util.*;
 import java.util.concurrent.Callable;
+
+import org.apache.commons.lang3.tuple.Pair;
+import util.Profiler;
 
 @Slf4j
 @Command(name = "DBTest", mixinStandardHelpOptions = true, version = "DBTest 0.1", description = "Test database isolation level.")
@@ -65,6 +67,7 @@ public class Main implements Callable<Integer> {
             return;
         }
 
+        Profiler profiler = Profiler.getInstance();
         // generate history
         int nHist = Integer.parseInt(config.getProperty("workload.history"));
         for (int i = 1; i <= nHist; i++) {
@@ -77,17 +80,51 @@ public class Main implements Callable<Integer> {
 
             // verify history
             log.info("Start history verification");
+            profiler.startTick("checker_performance");
             boolean result = checker.getDeclaredConstructor().newInstance().verify(history);
+            profiler.endTick("checker_performance");
             if (result) {
                 log.info("find bug");
             }
         }
         // collect runtime data
+        String session = config.getProperty("workload.session");
+        exportToCSV(session, profiler.getDurations(), profiler.getMaxMemory());
 
+    }
+
+    @SneakyThrows
+    public void exportToCSV(String session, Collection<Pair<String, Long>> durations, long maxMemory) {
+        List<String> lines = new ArrayList<>();
+        for (var p : durations) {
+            lines.add(String.format("%s,%s,%s\n", session, p.getValue(), Utils.formatMemory(maxMemory)));
+        }
+
+        // output lines to a csv file
+        File file = new File("./result/runtime.csv");
+        FileWriter csvWriter = new FileWriter(file, true);
+        for (String line : lines) {
+            csvWriter.write(line);
+        }
+        csvWriter.close();
     }
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new Main()).execute(args);
         System.exit(exitCode);
+    }
+}
+
+class Utils {
+    static String formatMemory(Long memoryBytes) {
+        double[] scale = { 1, 1024, 1024 * 1024, 1024 * 1024 * 1024 };
+        String[] unit = { "B", "KB", "MB", "GB" };
+
+        for (int i = scale.length - 1; i >= 0; i--) {
+            if (i == 0 || memoryBytes >= scale[i]) {
+                return String.format("%.1f", memoryBytes / scale[i]);
+            }
+        }
+        throw new Error("should not be here");
     }
 }
