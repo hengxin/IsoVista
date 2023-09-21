7dd969052dbf98bd69cf566fa37b2d2f53c9708d
Draco <dracode01@gmail.com>
2023-09-21 14:34:05 +0800

    Support more distribution for history generation (#25)

--- src/main/java/generator/general/GeneralGenerator.java
+++ src/main/java/generator/general/GeneralGenerator.java
@@ -4,31 +4,44 @@ import generator.Generator;
 import history.History;
 import history.Operation;
 import lombok.SneakyThrows;
-import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.math3.distribution.IntegerDistribution;
+import org.apache.commons.math3.distribution.UniformIntegerDistribution;
+import org.apache.commons.math3.distribution.ZipfDistribution;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class GeneralGenerator implements Generator<Long, Long> {
-    private long session;
-    private long transaction;
-    private long operation;
-    private double readProportion;
-    private long key;
-    private String distribution;
-    private Random random;
+    private final long session;
+    private final long transaction;
+    private final long operation;
+    private final double readProportion;
+    private final long key;
+    private IntegerDistribution keyDistribution;
+    private final BernoulliDistribution readProbability;
 
-    public GeneralGenerator(Properties config)  {
+    public GeneralGenerator(Properties config) {
         this.session = Long.parseLong(config.getProperty("workload.session"));
         this.transaction = Long.parseLong(config.getProperty("workload.transaction"));
         this.operation = Long.parseLong(config.getProperty("workload.operation"));
         this.readProportion = Double.parseDouble(config.getProperty("workload.readproportion"));
         this.key = Long.parseLong(config.getProperty("workload.key"));
-        this.distribution = config.getProperty("workload.distribution");
-        this.random = new Random();
+        var distribution = config.getProperty("workload.distribution");
+        switch (distribution) {
+            case "uniform":
+                this.keyDistribution = new UniformIntegerDistribution(1, (int) key);
+                break;
+            case "zipf":
+                this.keyDistribution = new ZipfDistribution((int) key, 1.5);
+                break;
+            case "hotspot":
+                this.keyDistribution = new HotspotIntegerDistribution(1, (int) key, 0.2);
+        }
+        this.readProbability = new BernoulliDistribution(readProportion);
     }
 
     @SneakyThrows
@@ -49,14 +62,13 @@ public class GeneralGenerator implements Generator<Long, Long> {
                     }
                     for (var iOp = 0; iOp < operation; iOp++) {
                         Operation.Type type;
-                        if (random.nextDouble() < readProportion) {
+                        if (readProbability.sample()) {
                             type = Operation.Type.READ;
                         } else {
                             type = Operation.Type.WRITE;
                         }
-                        // TODO: impl distribution
-                        var var = RandomUtils.nextLong(1, key + 1);
-                        var val = 0L;
+                        long var = keyDistribution.sample();
+                        long val = 0L;
                         if (type == Operation.Type.WRITE) {
                             val = counts.getOrDefault(var, 0L);
                             counts.put(var, val + 1);
