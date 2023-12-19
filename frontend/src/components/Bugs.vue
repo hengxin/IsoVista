44cbc0dffb73eac40a0bc6bab74c6110d3faf124
NayameoW <naya1207445468@foxmail.com>
2023-12-19 23:07:48 +0800

    add bug table

--- frontend/src/components/Bugs.vue
+++ frontend/src/components/Bugs.vue
@@ -7,34 +7,24 @@ defineProps({
     required: true
   }
 })
+const tableData = []
 </script>
 
 <template>
   <el-container class="layout-container-demo" style="height: 100%">
-    <el-header style="text-align: right; font-size: 12px">
-      <div class="toolbar">
-        <el-dropdown>
-          <el-icon style="margin-right: 8px; margin-top: 1px"
-          ><setting
-          /></el-icon>
-          <template #dropdown>
-            <el-dropdown-menu>
-              <el-dropdown-item>View</el-dropdown-item>
-              <el-dropdown-item>Add</el-dropdown-item>
-              <el-dropdown-item>Delete</el-dropdown-item>
-            </el-dropdown-menu>
-          </template>
-        </el-dropdown>
-        <span>Tom</span>
-      </div>
+    <el-header style="text-align: left; font-size: 22px">
+      All Bugs
     </el-header>
 
     <el-main>
       <el-scrollbar>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="Date" width="140" />
-          <el-table-column prop="name" label="Name" width="120" />
-          <el-table-column prop="address" label="Address" />
+        <el-table :data="tableData"
+                  :default-sort="{ prop: 'date', order: 'descending' }"
+                  stripe>
+          <el-table-column prop="filename" label="File Name" width="400"/>
+          <el-table-column prop="isoLevel" label="Isolation Level" width="220"/>
+          <el-table-column prop="checker" label="Checker" width="200"/>
+          <el-table-column prop="date" label="Date" sortable/>
         </el-table>
       </el-scrollbar>
     </el-main>
@@ -46,11 +36,13 @@ defineProps({
   position: relative;
   background-color: var(--el-color-primary-light-7);
   color: var(--el-text-color-primary);
+  margin-bottom: 20px;
 }
 
 .layout-container-demo .el-main {
   padding: 0;
 }
+
 .layout-container-demo .toolbar {
   display: inline-flex;
   align-items: center;
