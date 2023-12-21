1da15e0d5d94fa1cb62db4ca4ae6969effa3df8f
draco <dracode01@gmail.com>
2023-12-21 13:58:50 +0800

    Fix Dashboard.vue to match parameters with backend

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -10,14 +10,19 @@ defineProps({
 import {reactive} from "vue";
 
 const testingOption = reactive({
-  db_url: '',
+  db_url: 'jdbc:mysql://localhost:3306',
   db_type: '',
   db_isolation: '',
+  db_username: 'dbtest',
+  db_password: 'dbtest_pwd',
+  workload_type: 'general',
   workload_history: 10,
   workload_session: 20,
   workload_transaction: 100,
   workload_operation: 5,
   workload_key: 1000,
+  workload_readproportion: 0.5,
+  workload_distribution: '',
   checker_type: '',
   checker_isolation: '',
   profiler_enable: false
@@ -28,16 +33,27 @@ const dbOptions = [
   {label: 'PostgreSQL', value: 'POSTGRES'},
   {label: 'H2', value: 'H2'}
 ];
-const isolationLevelOptions = [
+const dbIsolationLevelOptions = [
   {label: 'Read_Uncommitted', value: 'READ_UNCOMMITTED'},
   {label: 'Read_Committed', value: 'READ_COMMITTED'},
   {label: 'Repeatable_Read', value: 'REPEATABLE_READ'},
   {label: 'Serializable', value: 'SERIALIZABLE'},
   {label: 'Transaction_Snapshot', value: 'TRANSACTION_SNAPSHOT'},
 ];
+const distributionOptions = [
+  {label: 'Uniform', value: 'uniform'},
+  {label: 'Zipf', value: 'zipf'},
+  {label: 'Hotspot', value: 'hotspot'},
+];
 const checkerOptions = [
   {label: 'PolySI', value: 'PolySI'},
   {label: 'C4', value: 'C4'}
+];
+const checkerIsolationLevelOptions = [
+  {label: 'Read_Committed', value: 'READ_COMMITTED'},
+  {label: 'Read_Atomicity', value: 'READ_ATOMICITY'},
+  {label: 'Causal_Consistency', value: 'CAUSAL_CONSISTENCY'},
+  {label: 'Snapshot_Isolation', value: 'SNAPSHOT_ISOLATION'},
 ];
 const handleSelectionChange = (value: string) => {
   console.log('Selected:', value);
@@ -98,52 +114,83 @@ async function handleSubmit() {
               <el-select v-model="testingOption.db_isolation" placeholder="" @change="handleSelectionChange"
                          class="fixed-width">
                 <el-option
-                    v-for="option in isolationLevelOptions"
+                    v-for="option in dbIsolationLevelOptions"
                     :key="option.value"
                     :label="option.label"
                     :value="option.value">
                 </el-option>
               </el-select>
             </el-form-item>
+            <el-form-item label="Username">
+              <el-input v-model="testingOption.db_username" placeholder="DB username" clearable class="fixed-width"></el-input>
+            </el-form-item>
+            <el-form-item label="Password">
+              <el-input v-model="testingOption.db_password" placeholder="DB password" clearable class="fixed-width"></el-input>
+            </el-form-item>
             <el-form-item label="#History">
               <el-input-number
                   v-model="testingOption.workload_history"
-                  :min="10"
+                  :min="1"
                   :max="100"
                   controls-position="right"
+                  class="fixed-width"
               />
             </el-form-item>
             <el-form-item label="#Session">
               <el-input-number
                   v-model="testingOption.workload_session"
-                  :min="20"
-                  :max="100"
+                  :min="1"
+                  :max="1000"
                   controls-position="right"
+                  class="fixed-width"
               />
             </el-form-item>
             <el-form-item label="#Txn/Sess">
               <el-input-number
                   v-model="testingOption.workload_transaction"
-                  :min="100"
-                  :max="200"
+                  :min="1"
+                  :max="1000"
                   controls-position="right"
+                  class="fixed-width"
               />
             </el-form-item>
             <el-form-item label="#Op/Txn">
               <el-input-number
                   v-model="testingOption.workload_operation"
-                  :min="5"
-                  :max="10"
+                  :min="1"
+                  :max="1000"
                   controls-position="right"
+                  class="fixed-width"
               />
             </el-form-item>
             <el-form-item label="#Key">
               <el-input-number
                   v-model="testingOption.workload_key"
-                  :min="1000"
-                  :max="2000"
+                  :min="1"
+                  :max="100000000"
                   controls-position="right"
+                  class="fixed-width"
               />
+            </el-form-item>
+            <el-form-item label="Read Proportion">
+              <el-input-number
+                  v-model="testingOption.workload_readproportion"
+                  :min="0"
+                  :max="1"
+                  controls-position="right"
+                  class="fixed-width"
+              />
+            </el-form-item>
+            <el-form-item label="Distribution">
+              <el-select v-model="testingOption.workload_distribution" placeholder="" @change="handleSelectionChange"
+                         class="fixed-width">
+                <el-option
+                    v-for="option in distributionOptions"
+                    :key="option.value"
+                    :label="option.label"
+                    :value="option.value">
+                </el-option>
+              </el-select>
             </el-form-item>
             <el-form-item label="Checker Type">
               <el-select v-model="testingOption.checker_type" placeholder="" @change="handleSelectionChange"
@@ -156,12 +203,12 @@ async function handleSubmit() {
                 </el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="DB Isolation Level">
+            <el-form-item label="Checker Isolation Level">
               <el-select v-model="testingOption.checker_isolation" placeholder=""
                          @change="handleSelectionChange"
                          class="fixed-width">
                 <el-option
-                    v-for="option in isolationLevelOptions"
+                    v-for="option in checkerIsolationLevelOptions"
                     :key="option.value"
                     :label="option.label"
                     :value="option.value">
@@ -203,5 +250,7 @@ async function handleSubmit() {
 .fixed-width {
   width: 600px;
 }
+
+.el-input .el-input__inner {text-align:left}
 
 </style>
