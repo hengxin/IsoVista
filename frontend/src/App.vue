975a553a02a01d3787d36dbb83007cff3555765f
NayameoW <naya1207445468@foxmail.com>
2023-12-22 00:45:10 +0800

    extract sidebar from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,61 +1,15 @@
 <template>
   <el-container class="layout-container-demo" style="height: 100%">
-    <el-aside width="200px">
-      <el-scrollbar>
-        <h5 class="mb-2">DBTest</h5>
-        <el-menu
-            class="el-menu-vertical-demo"
-            default-active="1"
-        >
-          <el-menu-item index="1" @click="$router.push('/dashboard')">
-            <template #title>
-              <el-icon>
-                <Setting />
-              </el-icon>
-              <span>Dashboard</span>
-            </template>
-          </el-menu-item>
-          <el-menu-item index="2" @click="$router.push('/runs')">
-            <template #title>
-              <el-icon>
-                <List />
-              </el-icon>
-              <span>Runs</span>
-            </template>
-          </el-menu-item>
-          <el-menu-item index="3" @click="$router.push('/bugs')">
-            <template #title>
-              <el-icon>
-                <Failed />
-              </el-icon>
-              <span>Bugs</span>
-            </template>
-          </el-menu-item>
-        </el-menu>
-      </el-scrollbar>
-    </el-aside>
-    <el-container>
-      <router-view></router-view>
-    </el-container>
-
+    <Sidebar />
+    <router-view></router-view>
   </el-container>
 </template>
 
 <script lang="ts" setup>
-import {House, Failed, View, Setting, List} from '@element-plus/icons-vue'
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
 
 </script>
 
 <style scoped>
-.el-menu-vertical-demo {
-  height: 100vh;
-}
 
-.layout-container-demo .toolbar {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  right: 20px;
-}
 </style>
---
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+import {Failed, List, Setting} from "@element-plus/icons-vue";
+
+</script>
+
+<template>
+  <div class="Sidebar" >
+    <el-aside width="200px">
+      <el-scrollbar>
+        <h5 class="mb-2">DBTest</h5>
+        <el-menu
+            class="el-menu-vertical-demo"
+            default-active="1"
+        >
+          <el-menu-item index="1" @click="$router.push('/dashboard')">
+            <template #title>
+              <el-icon>
+                <Setting />
+              </el-icon>
+              <span>Dashboard</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="2" @click="$router.push('/runs')">
+            <template #title>
+              <el-icon>
+                <List />
+              </el-icon>
+              <span>Runs</span>
+            </template>
+          </el-menu-item>
+          <el-menu-item index="3" @click="$router.push('/bugs')">
+            <template #title>
+              <el-icon>
+                <Failed />
+              </el-icon>
+              <span>Bugs</span>
+            </template>
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+  </div>
+
+
+</template>
+
+<style scoped>
+.el-menu-vertical-demo {
+  height: 100vh;
+}
+</style>
