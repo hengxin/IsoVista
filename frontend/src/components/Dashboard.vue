6bfd7c4c24173f62505c143c86e98220f063f219
NayameoW <naya1207445468@foxmail.com>
2023-12-28 00:39:09 +0800

    update dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import {reactive, ref} from "vue";
-import {ElLoading, ElMessage, ElMessageBox} from "element-plus";
-import type {UploadProps, UploadUserFile} from 'element-plus'
-import {run} from "@/api/api";
+import {reactive, ref} from "vue"
+import {ElLoading, ElMessage, ElMessageBox} from "element-plus"
+import type {UploadProps, UploadUserFile} from "element-plus"
+import {run} from "@/api/api"
 
 const testingOption = reactive({
   db_url: 'jdbc:mysql://localhost:3306',
@@ -58,6 +58,7 @@ const handleSwitch = (value: boolean) => {
   console.log('Enabled: ', value);
 };
 
+
 async function handleSubmit() {
   console.log(testingOption)
   const loading = ElLoading.service({
@@ -108,6 +109,12 @@ const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
       () => false
   )
 }
+
+const dialogVisible = ref(false)
+const activeIndex = ref(0)
+const handleIndexChange = (index) => {
+  activeIndex.value = index
+}
 </script>
 
 <template>
@@ -115,205 +122,249 @@ const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
     <el-header></el-header>
 
     <el-main>
-      <el-row>
-        <el-col :span="2">
-        </el-col>
-        <el-col :span="9">
-          <el-form label-position="left"
-                   label-width="220px"
-                   class="form-style"
-          >
-            <header class="form-header">
-              <h2>Database Settings</h2>
-            </header>
-            <el-form-item label="DB URL">
-              <el-input v-model="testingOption.db_url" placeholder="JDBC URL" clearable class="fixed-width"></el-input>
-            </el-form-item>
-            <el-form-item label="DB Type">
-              <el-select v-model="testingOption.db_type" placeholder="" @change="handleSelectionChange"
-                         class="fixed-width">
-                <el-option
-                    v-for="option in dbOptions"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="DB Isolation Level">
-              <el-select v-model="testingOption.db_isolation" placeholder="" @change="handleSelectionChange"
-                         class="fixed-width">
-                <el-option
-                    v-for="option in dbIsolationLevelOptions"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Username">
-              <el-input v-model="testingOption.db_username" placeholder="DB username" clearable
-                        class="fixed-width"></el-input>
-            </el-form-item>
-            <el-form-item label="Password">
-              <el-input v-model="testingOption.db_password" placeholder="DB password" clearable
-                        class="fixed-width"></el-input>
-            </el-form-item>
-          </el-form>
-        </el-col>
-        <el-col :span="2">
-        </el-col>
-        <el-col :span="9">
-          <el-form label-position="left"
-                   label-width="220px"
-                   class="form-style"
-          >
-            <header class="form-header">
-              <h2>Workload Settings</h2>
-            </header>
-            <el-form-item label="#History">
-              <el-input-number
-                  v-model="testingOption.workload_history"
-                  :min="0"
-                  :max="99"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="#Session">
-              <el-input-number
-                  v-model="testingOption.workload_session"
-                  :min="0"
-                  :max="999"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="#Txn/Sess">
-              <el-input-number
-                  v-model="testingOption.workload_transaction"
-                  :min="0"
-                  :max="999"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="#Op/Txn">
-              <el-input-number
-                  v-model="testingOption.workload_operation"
-                  :min="0"
-                  :max="999"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="#Key">
-              <el-input-number
-                  v-model="testingOption.workload_key"
-                  :min="0"
-                  :max="99999999"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="Read Proportion">
-              <el-input-number
-                  v-model="testingOption.workload_readproportion"
-                  :min="-1"
-                  :max="0"
-                  controls-position="right"
-                  class="fixed-width"
-              />
-            </el-form-item>
-            <el-form-item label="Distribution">
-              <el-select v-model="testingOption.workload_distribution" placeholder="" @change="handleSelectionChange"
-                         class="fixed-width">
-                <el-option
-                    v-for="option in distributionOptions"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </el-form>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="2">
-        </el-col>
-        <el-col :span="9">
-
-          <el-form  label-position="left"
-                    label-width="220px"
-                    class="form-style">
-            <header class="form-header">
-              <h2>Checker Settings</h2>
-            </header>
-            <el-form-item label="Checker Type">
-              <el-select v-model="testingOption.checker_type" placeholder="" @change="handleSelectionChange"
-                         class="fixed-width">
-                <el-option
-                    v-for="option in checkerOptions"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Checker Isolation Level">
-              <el-select v-model="testingOption.checker_isolation" placeholder=""
-                         @change="handleSelectionChange"
-                         class="fixed-width">
-                <el-option
-                    v-for="option in checkerIsolationLevelOptions"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value">
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-divider/>
-            <el-form-item label="Enable Profiler">
-              <el-switch
-                  v-model="testingOption.profiler_enable"
-                  class="ml-2"
-                  @change="handleSwitch"
-              />
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="handleSubmit">start</el-button>
-            </el-form-item>
-            <el-divider></el-divider>
-            <el-form-item>
-              <el-upload
-                  v-model:file-list="fileList"
-                  class="upload-demo"
-                  action=""
-                  multiple
-                  :on-preview="handlePreview"
-                  :on-remove="handleRemove"
-                  :before-remove="beforeRemove"
-                  :limit="3"
-                  :on-exceed="handleExceed"
+      <el-row :gutter="20">
+        <el-col :span="16">
+          <el-carousel trigger="click"
+                       type="card"
+                       :autoplay="false"
+                       height="900px"
+                       direction="vertical"
+                       arrow="always"
+                       @change="handleIndexChange">
+            <el-carousel-item>
+              <el-form label-position="left"
+                       label-width="200px"
+                       class="el-form-in-carousel"
               >
-                <el-button type="primary">Upload</el-button>
-                <template #tip>
-                  <div class="el-upload__tip">
-                    upload your generated history file here
-                  </div>
-                </template>
-              </el-upload>
-            </el-form-item>
-          </el-form>
+                <header class="form-header">
+                  <h2>Database Settings</h2>
+                </header>
+                <el-form-item label="DB URL">
+                  <el-input v-model="testingOption.db_url" placeholder="JDBC URL" clearable
+                            class="fixed-width"></el-input>
+                </el-form-item>
+                <el-form-item label="DB Type">
+                  <el-select v-model="testingOption.db_type" placeholder="" @change="handleSelectionChange"
+                             class="fixed-width">
+                    <el-option
+                        v-for="option in dbOptions"
+                        :key="option.value"
+                        :label="option.label"
+                        :value="option.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="DB Isolation Level">
+                  <el-select v-model="testingOption.db_isolation" placeholder="" @change="handleSelectionChange"
+                             class="fixed-width">
+                    <el-option
+                        v-for="option in dbIsolationLevelOptions"
+                        :key="option.value"
+                        :label="option.label"
+                        :value="option.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="Username">
+                  <el-input v-model="testingOption.db_username" placeholder="DB username" clearable
+                            class="fixed-width"></el-input>
+                </el-form-item>
+                <el-form-item label="Password">
+                  <el-input v-model="testingOption.db_password" placeholder="DB password" clearable
+                            class="fixed-width"></el-input>
+                </el-form-item>
+              </el-form>
+            </el-carousel-item>
+            <el-carousel-item>
+              <el-form label-position="left"
+                       label-width="220px"
+                       class="el-form-in-carousel"
+              >
+                <header class="form-header">
+                  <h2>Workload Settings</h2>
+                </header>
+                <el-form-item label="#History">
+                  <el-input-number
+                      v-model="testingOption.workload_history"
+                      :min="0"
+                      :max="99"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="#Session">
+                  <el-input-number
+                      v-model="testingOption.workload_session"
+                      :min="0"
+                      :max="999"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="#Txn/Sess">
+                  <el-input-number
+                      v-model="testingOption.workload_transaction"
+                      :min="0"
+                      :max="999"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="#Op/Txn">
+                  <el-input-number
+                      v-model="testingOption.workload_operation"
+                      :min="0"
+                      :max="999"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="#Key">
+                  <el-input-number
+                      v-model="testingOption.workload_key"
+                      :min="0"
+                      :max="99999999"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="Read Proportion">
+                  <el-input-number
+                      v-model="testingOption.workload_readproportion"
+                      :min="-1"
+                      :max="0"
+                      controls-position="right"
+                      class="fixed-width"
+                  />
+                </el-form-item>
+                <el-form-item label="Distribution">
+                  <el-select v-model="testingOption.workload_distribution" placeholder=""
+                             @change="handleSelectionChange"
+                             class="fixed-width">
+                    <el-option
+                        v-for="option in distributionOptions"
+                        :key="option.value"
+                        :label="option.label"
+                        :value="option.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-form>
+            </el-carousel-item>
+            <el-carousel-item>
+              <el-form label-position="left"
+                       label-width="220px"
+                       class="el-form-in-carousel">
+                <header class="form-header">
+                  <h2>Checker Settings</h2>
+                </header>
+                <el-form-item label="Checker Type">
+                  <el-select v-model="testingOption.checker_type" placeholder="" @change="handleSelectionChange"
+                             class="fixed-width">
+                    <el-option
+                        v-for="option in checkerOptions"
+                        :key="option.value"
+                        :label="option.label"
+                        :value="option.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="Checker Isolation Level">
+                  <el-select v-model="testingOption.checker_isolation" placeholder=""
+                             @change="handleSelectionChange"
+                             class="fixed-width">
+                    <el-option
+                        v-for="option in checkerIsolationLevelOptions"
+                        :key="option.value"
+                        :label="option.label"
+                        :value="option.value">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-divider/>
+                <el-form-item label="Enable Profiler">
+                  <el-switch
+                      v-model="testingOption.profiler_enable"
+                      class="ml-2"
+                      @change="handleSwitch"
+                  />
+                </el-form-item>
+                <el-form-item>
+                </el-form-item>
+                <el-form-item>
+                  <el-upload
+                      v-model:file-list="fileList"
+                      class="upload-demo"
+                      action=""
+                      multiple
+                      :on-preview="handlePreview"
+                      :on-remove="handleRemove"
+                      :before-remove="beforeRemove"
+                      :limit="3"
+                      :on-exceed="handleExceed"
+                  >
+                    <el-button type="primary">Upload</el-button>
+                    <template #tip>
+                      <div class="el-upload__tip">
+                        upload your generated history file here
+                      </div>
+                    </template>
+                  </el-upload>
+                </el-form-item>
+              </el-form>
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+        <el-col :span="8">
+          <el-card class="box-card" v-if="activeIndex===0">
+            <template #header>
+              <div class="card-header">
+                <span>Database Setting Details</span>
+              </div>
+              <p> url: your jdbc url to connect to the database</p>
+              <p> username: the username of the database</p>
+              <p> password: the password of the database</p>
+              <p> isolation level: stub, currently all history are generated under serialization </p>
+              <p> database type: support MYSQL, POSTGRES and H2</p>
+            </template>
+          </el-card>
+          <el-card class="box-card" v-if="activeIndex===1">
+            <template #header>
+              <div class="card-header">
+                <span>Workload Setting Details</span>
+              </div>
+            </template>
+          </el-card>
+          <el-card class="box-card" v-if="activeIndex===2">
+            <template #header>
+              <div class="card-header">
+                <span>Checker Setting Details</span>
+              </div>
+
+            </template>
+          </el-card>
         </el-col>
       </el-row>
+      <el-button type="primary" @click="dialogVisible = true">start</el-button>
+      <el-dialog
+          v-model="dialogVisible"
+          title="Option Overview"
+          width="50%"
+      >
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button @click="dialogVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="dialogVisible = false">
+              Confirm
+            </el-button>
+          </span>
+        </template>
+      </el-dialog>
     </el-main>
   </el-container>
 </template>
 
-<style>
+<style scoped>
 
 .fixed-width {
   width: 600px;
@@ -333,4 +384,43 @@ const beforeRemove: UploadProps['beforeRemove'] = (uploadFile, uploadFiles) => {
   margin-bottom: 20px;
   text-align: left;
 }
+
+
+.el-carousel {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.el-carousel__container {
+  display: flex;
+  align-items: center;
+}
+
+.el-form-in-carousel {
+  max-width: 100%;
+  max-height: 100%;
+  border: 1px solid transparent;
+  border-radius: 15px;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: rgb(236, 236, 236);
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+}
+
 </style>
