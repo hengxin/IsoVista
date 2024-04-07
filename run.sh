0cb3c1c3956453ad40456ffe74b8324e7289a0dc
draco <dracode01@gmail.com>
2024-04-07 21:45:08 +0800

    Fix typo
    Signed-off-by: draco <dracode01@gmail.com>

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ -z "${VITE_BACKEND_URL}" ]; then
     echo "VITE_BACKEND_URL is not set. Default to http://127.0.0.1:8000/"
 else
     # Replace the backend url with VITE_BACKEND_URL
-    echo "VITE_BACKEND_URL is set to ${VITE_BACKEND_URL}。"
+    echo "VITE_BACKEND_URL is set to ${VITE_BACKEND_URL}."
 
     # Find and replace in index-*.js files within /app/dist/asserts/
     find /app/dist/assets/ -type f -name 'index-*.js' -exec sed -i "s|http://127.0.0.1:8000/|$VITE_BACKEND_URL|g" {} \;
