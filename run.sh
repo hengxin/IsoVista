1a7d89c15403b7cd5746ab9786f34aac386a7de0
draco <dracode01@gmail.com>
2024-04-07 20:27:37 +0800

    Update docker startup script
    Signed-off-by: draco <dracode01@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check if the environment variable VITE_BACKEND_URL is set
+if [ -z "${VITE_BACKEND_URL}" ]; then
+    echo "VITE_BACKEND_URL is not set. Default to http://127.0.0.1:8000/"
+else
+    # Replace the backend url with VITE_BACKEND_URL
+    echo "VITE_BACKEND_URL is set to ${VITE_BACKEND_URL}。"
+
+    # Find and replace in index-*.js files within /app/dist/asserts/
+    find /app/dist/asserts/ -type f -name 'index-*.js' -exec sed -i "s|http://127.0.0.1:8000/|$VITE_BACKEND_URL|g" {} \;
+
+    echo "Backend url replacement complete."
+fi
+
+
 # Start the frontend process
 http-server dist &
 
